57bbe78154ceb629da1b2253eb9b4b5768492f67
Proton4ik <dad-192@mail.ru>
2021-10-12 01:47:39 +0300

    s

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'geekshop/index.html')
+    title = 'Магазин'
+
+    context = {
+        'title': title,
+
+    }
+    return render(request, 'geekshop/index.html', context )
 
 
 def contacts(request):
-    return render(request, 'geekshop/contacts.html')
+    title = 'Контакты'
+
+    context = {
+        'title': title,
+
+    }
+    return render(request, 'geekshop/contacts.html', context)
