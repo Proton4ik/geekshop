25e2254ef888cbc38bf9dfd786f7f99465995895
Proton4ik <dad-192@mail.ru>
2021-10-15 14:29:15 +0300

    44

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('', main, name='main'),
     path('contacts/', contacts, name='contacts'),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
+
 ]
 
 if settings.DEBUG:
