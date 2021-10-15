a991b2ca464f1aaab773e9c86fc1010cc1b321b9
Proton4ik <dad-192@mail.ru>
2021-10-15 14:38:53 +0300

    444

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 
 
 def login(request):
@@ -27,3 +27,37 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main'))
+
+
+def register(request):
+    title = 'регистрация'
+
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {'title': title, 'register_form': register_form}
+
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    title = 'редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {'title': title, 'edit_form': edit_form}
+
+    return render(request, 'authapp/edit.html', context)
+
